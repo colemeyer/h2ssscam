07486b5d7f3c8c0b2820fa727281d39bc6910d42
Cole <cmmeyer@arizona.edu>
2025-08-04 16:17:55 -0500

    Add speed testing comments on most intensive lines.

--- h2ssscam/h2_model.py
+++ h2ssscam/h2_model.py
@@ -11,6 +11,7 @@ import astropy.units as u
 from h2_model_funcs import *
 from h2_model_params import *
 import numpy as np
+import time
 # from funkyfresh import set_style
 # set_style('AAS', silent=True)
 
@@ -21,6 +22,7 @@ import numpy as np
 # Abgrall et al. (1993) fluorescence line list
 s = np.load('h2fluor_data_Abgrall+1993.npz', allow_pickle=True)
 Atot, Auldiss, Aul, lamlu, band, vu, ju, vl, jl = s['Atot'] * u.s**-1, s['Auldiss'] * u.s**-1, s['Aul'] * u.s**-1, s['lamlu'] * u.AA, s['band'], s['vu'], s['ju'], s['vl'], s['jl']
+
 
 # Filter by v <= VMAX, J <= JMAX
 mask_h2 = (vl <= VMAX) & (jl <= JMAX)
@@ -45,13 +47,16 @@ ERG_UNIT = u.erg * u.cm**-2 * u.s**-1 * u.arcsec**-2 * u.nm**-1
 if UNIT == 'CU': UNIT = CU_UNIT
 else: UNIT = ERG_UNIT
 
+
 # Wavelength grid from 912 to 1800 Å (dlam = 0.01 angstroms)
 lam0, lamend, dlam = 912, 1800, 0.1
 lam = np.linspace(int(lam0), int(lamend), int((lamend - lam0) / dlam)) * u.AA
 
+
 # Compute Doppler widths
 dv_phys = calc_dv(T=TH2)               # thermal + non-thermal (Eq. 7)
 dv_tot = calc_dv(T=TH2, R=RESOLVING_POWER, instr=True)      # include instrumental
+
 
 # H2 oscillator strengths and level populations
 flu = calc_flu(ju, jl, lamlu, Aul)  # Eq. 2
@@ -59,6 +64,7 @@ nvj = calc_nvj(NH2_TOT, TH2)        # Eq. 8
 sel_levels = np.where(nvj[vl,jl] > NH2_CUTOFF)[0]
 Atot_p, Auldiss_p, Aul_p, lamlu_p, band_p, vu_p, ju_p, vl_p, jl_p, flu_p = Atot[sel_levels], Auldiss[sel_levels], Aul[sel_levels], lamlu[sel_levels], band[sel_levels], vu[sel_levels], ju[sel_levels], vl[sel_levels], jl[sel_levels], flu[sel_levels]
 nvj_p = nvj[vl_p, jl_p]
+
 
 # HI calculations
 NHI = boltzmann(NHI_TOT, hi_ju, hi_jl, hi_lamlu, THI)
@@ -70,19 +76,23 @@ hih2_lamlu, hih2_flu, hih2_Atot, hih2_N = np.append(hi_lamlu, lamlu_p), np.appen
 # ------------------------------------------------------ #
 
 # Compute absorption cross-sections and optical depths
+# SPEED TESTING: following 3 lines took ~0.6 seconds to run on 2023 Mac Pro M3 Pro chip
 siglu = calc_siglu(lam, hih2_lamlu, hih2_Atot, dv_phys, hih2_flu)  # Eq. 4
 tau = calc_tau(hih2_N, siglu)                                 # Eq. 11
 tau_tot = tau.sum(axis=0)                                    # total tau(lambda)
+
 
 # Incident UV background and attenuated source
 if INC_SOURCE == 'BLACKBODY': uv_inc = blackbody(lam, THI, unit=UNIT)
 else: uv_inc = uv_continuum(lam, unit=UNIT)                                   # empirical cont.
 source = uv_inc * np.exp(-tau_tot)
 
+
 # Absorption rates for H2 only
 tau_h2 = tau[len(hi_lamlu):, :]
 abs_rate = calc_abs_rate(uv_inc, tau_h2, tau_tot, unit=UNIT) * dlam     # Eq. 12–13
 abs_rate_per_trans = np.sum(abs_rate, axis=1)
+
 
 # Plot source spectrum
 plt.figure()
@@ -99,6 +109,7 @@ plt.show()
 # ------------------------------------------------------ #
 
 # Assemble arrays for relevant emission lines
+# SPEED TESTING: following 14 lines took ~1.7 seconds to run on 2023 Mac Pro M3 Pro chip
 vljl = []
 h2_lamlu = []
 h2_Atot = []
@@ -122,11 +133,15 @@ lam0, lamend, dlam = 912, 1800, DLAM.to(u.AA).value
 lam_highres = np.linspace(int(lam0), int(lamend), int((lamend - lam0) / dlam)) * u.AA
 source = np.interp(lam_highres, lam, source)
 
+
 ### Calculate emergent spectrum
+# SPEED TESTING: following line took ~5.3 seconds to run on 2023 Mac Pro M3 Pro chip
 lam_shifted, spec, spec_tot = calc_spec(lam_highres, h2_lamlu, h2_Atot, dv_tot, flux_per_trans, source, UNIT, DOPPLER_SHIFT)
+
 
 ### Save emergent spectrum
 np.savez_compressed(f'models/h2-fluor-model_R={RESOLVING_POWER}_TH2={int(TH2.value)}_NH2={int(np.log10(NH2_TOT.value))}_THI={int(THI.value)}_NHI={int(np.log10(NHI_TOT.value))}', lam_shifted=lam_shifted, spec=spec.to(UNIT).value, spec_tot=spec_tot.to(UNIT).value)
+
 
 # Plot emission-only spectrum
 plt.plot(lam_shifted, spec, c='k', lw=0.5)
@@ -137,6 +152,7 @@ else: plt.ylabel(r'Intensity ($\mathrm{erg\;cm^{-2}\;s^{-1}\;arcsec^{-1}\;nm^{-1
 plt.xlim([BP_MIN.value, BP_MAX.value])
 plt.title('Emergent Spectrum')
 plt.show()
+
 
 # Plot total (emission + continuum) spectrum
 plt.plot(lam_shifted, spec_tot, c='k', lw=0.5)
