a1064a9e1d7bad2a110c3c23d0773fc69da6d6f1
dominp <dominik@pacholski.pw>
2025-08-06 10:43:34 -0500

    solved loading issue

--- src/h2ssscam/data_loader.py
+++ src/h2ssscam/data_loader.py
@@ -4,5 +4,5 @@ import importlib.resources
 
 def load_data(filename):
     with importlib.resources.files("h2ssscam.data").joinpath(f"{filename}.npz").open("rb") as f:
-        data = np.load(f)
-    return data
+        with np.load(f, allow_pickle=True) as data:
+            return dict(data)
