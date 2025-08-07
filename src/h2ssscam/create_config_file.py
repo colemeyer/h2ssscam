53fc5e0887d5611bf1801581eb1d0cec2003a9b0
dominp <dominik@pacholski.pw>
2025-08-07 17:23:02 -0500

    added filename to create_config_file

--- src/h2ssscam/create_config_file.py
+++ src/h2ssscam/create_config_file.py
@@ -2,21 +2,25 @@ from datetime import datetime
 import importlib.resources, os, sys
 
 
-def create_config_file(output_path: str):
+def create_config_file(output_path: str,file_name:str|None = None):
     """Creates config file based on the file with default parameters
 
     Parameters
     ----------
     output_path : str
         Directory in which file is created
+    file_name : str
+        Name of the config file, optional
     """
     if not isinstance(output_path, str):
         raise TypeError("Path has to be a string")
     timestamp = datetime.now().strftime("%y%m%d_%H%M%S")
     with importlib.resources.files("h2ssscam.data").joinpath(f"config.ini").open("r") as f:
         config_content = f.read()
-
-    output_file = os.path.join(output_path, f"config_{timestamp}.ini")
+    if file_name:
+        output_file = os.path.join(output_path, f"{file_name}.ini")
+    else:
+        output_file = os.path.join(output_path, f"config_{timestamp}.ini")
     with open(output_file, "w") as out:
         out.write(config_content)
     print(f"Config file saved as {output_file}")
@@ -24,5 +28,6 @@ def create_config_file(output_path: str):
 
 if __name__ == "__main__":
 
-    output = sys.argv[1] if len(sys.argv) > 1 else "."
-    create_config_file(output)
+    output_path = sys.argv[1] if len(sys.argv) > 1 else "."
+    output_filename = sys.argv[2] if len(sys.argv) > 2 else None
+    create_config_file(output_path,output_filename)
