9698cf24e52abf7fcb13831541ca105659b4cfeb
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2025-08-07 15:47:52 -0500

    Added an empty TestBaseCalc class

--- tests/test_BaseCalc.py
+++ tests/test_BaseCalc.py
@@ -1,1 +1,10 @@
+"""
+Contains the tests for the BaseCalc module.
+"""
 import pytest
+from h2ssscam.BaseCalc import BaseCalc
+
+class TestBaseCalc:
+    """
+    Contains the tests for the BaseCalc class.
+    """
