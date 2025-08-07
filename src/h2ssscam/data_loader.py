a05a61c60736590dcedd5308188f02e638b6ef7b
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2025-08-07 13:54:42 -0500

    Made style changes to load_data

--- src/h2ssscam/data_loader.py
+++ src/h2ssscam/data_loader.py
@@ -2,7 +2,22 @@ import numpy as np
 import importlib.resources
 
 
-def load_data(filename):
-    with importlib.resources.files("h2ssscam.data").joinpath(f"{filename}.npz").open("rb") as f:
-        with np.load(f, allow_pickle=True) as data:
-            return dict(data)
+def load_data(filename: str) -> dict:
+    """Returns the NPZ file data in a dict.
+
+    The data must be located in the 'src/h2ssscam/data' directory.
+
+    Args:
+        filename: the name of the file to fetch, but without the '.npz' suffix.
+
+    Returns:
+        A dict containing the data from the file.
+
+    Raises:
+        FileNotFoundError: The file could not be found in the 'src/h2ssscam/data' directory.
+    """
+    with (
+        importlib.resources.files("h2ssscam.data").joinpath(f"{filename}.npz").open("rb") as f,
+        np.load(f, allow_pickle=True) as data
+    ):
+        return dict(data)
