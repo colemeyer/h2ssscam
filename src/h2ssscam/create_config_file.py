29ae2b7dfa041049cce6ab207571137a3845218f
dominp <dominik@pacholski.pw>
2025-08-07 16:18:05 -0500

    added doc to create_config_file

--- src/h2ssscam/create_config_file.py
+++ src/h2ssscam/create_config_file.py
@@ -3,6 +3,15 @@ import importlib.resources, os, sys
 
 
 def create_config_file(output_path: str):
+    """Creates config file based on the file with default parameters
+
+    Parameters
+    ----------
+    output_path : str
+        Directory in which file is created
+    """
+    if not isinstance(output_path, str):
+        raise TypeError("Path has to be a string")
     timestamp = datetime.now().strftime("%y%m%d_%H%M%S")
     with importlib.resources.files("h2ssscam.data").joinpath(f"config.ini").open("r") as f:
         config_content = f.read()
