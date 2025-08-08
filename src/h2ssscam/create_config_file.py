0cf21aa47fa461628dc997577ddcccb0522618f7
Daniel Lopez-Sanders <lopezsandersdaniel@gmail.com>
2025-08-08 10:31:52 -0500

    Revert "added filename to create_config_file"
    This reverts commit 53fc5e0887d5611bf1801581eb1d0cec2003a9b0. Only the testing deletion was reverted.

--- src/h2ssscam/create_config_file.py
+++ src/h2ssscam/create_config_file.py
@@ -2,15 +2,13 @@ from datetime import datetime
 import importlib.resources, os, sys
 
 
-def create_config_file(output_path: str,file_name:str|None = None):
+def create_config_file(output_path: str):
     """Creates config file based on the file with default parameters
 
     Parameters
     ----------
     output_path : str
         Directory in which file is created
-    file_name : str
-        Name of the config file, optional
     """
     if not isinstance(output_path, str):
         raise TypeError("Path has to be a string")
@@ -19,12 +17,7 @@ def create_config_file(output_path: str,file_name:str|None = None):
     timestamp = datetime.now().strftime("%y%m%d_%H%M%S")
     with importlib.resources.files("h2ssscam.data").joinpath(f"config.ini").open("r") as f:
         config_content = f.read()
-    if file_name:
-        output_file = os.path.join(output_path, f"{file_name}.ini")
-        if os.path.isfile(output_file):
-            raise ValueError('File already exists!')
-    else:
-        output_file = os.path.join(output_path, f"config_{timestamp}.ini")
+    output_file = os.path.join(output_path, f"config_{timestamp}.ini")
     with open(output_file, "w") as out:
         out.write(config_content)
     print(f"Config file saved as {output_file}")
@@ -32,6 +25,5 @@ def create_config_file(output_path: str,file_name:str|None = None):
 
 if __name__ == "__main__":
 
-    output_path = sys.argv[1] if len(sys.argv) > 1 else "."
-    output_filename = sys.argv[2] if len(sys.argv) > 2 else None
-    create_config_file(output_path,output_filename)
+    output = sys.argv[1] if len(sys.argv) > 1 else "."
+    create_config_file(output)
